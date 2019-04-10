64a71a80f7c47067b05337e10d867d643aec090a
Kajal Sheth <ks4vp@virginia.edu>
2019-04-10 13:30:06 -0400

    login at least reirects

--- createaccount.php
+++ createaccount.php
@@ -10,7 +10,7 @@ $conn = new mysqli($host, $dbusername, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$conn->close();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -28,8 +28,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             echo "Exists";
         }
         else{
-            $sql = "INSERT INTO professor (first_name, last_name, university)
-            VALUES ('$first_name', '$last_name', '$username', 'password')";
+            $sql = "INSERT INTO login (first_name, last_name, username, password)
+            VALUES ('$first_name', '$last_name', '$username', '$password')";
             if ($conn->query($sql) === TRUE) {
                 header('location: http://localhost/WebPL/home.html');
             } else {
