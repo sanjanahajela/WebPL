7b34bd5f566487a5456c4d7e72dd490a4d35709e
Kajal Sheth <ks4vp@virginia.edu>
2019-04-10 12:24:30 -0400

    first name database check

--- add-professor.php
+++ add-professor.php
@@ -3,6 +3,7 @@
 
 <?php
 
+
 $host = 'localhost';
 $dbusername = 'ks4vp';
 $password = 'CSgods123';
@@ -20,30 +21,40 @@ if ($conn->connect_error) {
 
 
 
-
-
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $university = $_POST['university'];
 
-    $sql = "INSERT INTO professor (first_name, last_name, university)
-    VALUES ('$first_name', '$last_name', '$university')";
+    $query = "SELECT * from professor where first_name ='$first_name'";
+    #checks if the first name of a professor is in the database but you want to check all 
+    if ($result=mysqli_query($conn,$query))
+    {
+        if(mysqli_num_rows($result) > 0)
+        {
+            echo "Exists";
+        }
+        else{
+            $sql = "INSERT INTO professor (first_name, last_name, university)
+            VALUES ('$first_name', '$last_name', '$university')";
+            if ($conn->query($sql) === TRUE) {
+                header('location: http://localhost/WebPL/search.html');
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+        
+    }
+    else{
+        echo "Query Failed.";
 
-
-    if ($conn->query($sql) === TRUE) {
-        header('location: http://localhost/WebPL/search.html');
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
+        
+
+
+
     $conn->close();
-
-$conn->close();
-
-
-
 
     
 
