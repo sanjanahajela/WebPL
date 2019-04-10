6e9901f8cb21e74f13003718a5e5d6e1bff392ff
Sanjana <sh9as@virginia.edu>
2019-04-10 13:13:58 -0400

    yeet

--- createaccount.php
+++ createaccount.php
@@ -15,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $username = $_POST['username'];
+    $password= $_POST['password'];
     echo('hello')
     #$query = "INSERT * from login where first_name ='$first_name'";
     
