c6f2c65753682a517dc5923e708e4f8f36d4d4dc
Sanjana <sh9as@virginia.edu>
2019-04-10 11:35:13 -0400

    ewajfawief

---
+++ createaccount.php
@@ -0,0 +1,35 @@
+
+<?php
+
+$host = 'localhost';
+$dbusername = 'ks4vp';
+$password = 'CSgods123';
+$dbnmame = 'professors';
+$usertable= 'profiles'
+
+
+$db = new PDO ($dsn, $first_name, $last_name, $username, $password );
+
+
+catch(PDOException $e){
+    $error_message = $e -> getMessage();
+    echo "<p>An error occured while connecting to the database: $error_message </p>";
+
+}
+
+catch(Exception $e){
+    $error_message = $e -> getMessage();
+    echo "<p>Error message: $error_message </p>";
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $username= $_POST['username'];
+    $password= $_POST['password']
+
+
+    echo 'Thank you for creating an account!';
+}
+?>
