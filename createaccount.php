4c4ad6ecef8fde6773a841b355ac49287d29601f
Kajal Sheth <ks4vp@virginia.edu>
2019-04-12 00:51:48 -0400

    user database works

--- createaccount.php
+++ createaccount.php
@@ -1,9 +1,17 @@
+<!-- // Kajal Sheth(ks4vp) and Sanjana Hajela(sh9as)
+-->
+
 <?php
+
+
 $host = 'localhost';
 $dbusername = 'ks4vp';
 $password = 'CSgods123';
 $dbname = 'professor';
-$usertable="login";
+$usertable="user";
+$yourfield = "first_name";
+
+
 // Create connection
 $conn = new mysqli($host, $dbusername, $password, $dbname);
 // Check connection
@@ -11,14 +19,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $username = $_POST['username'];
-    $password= $_POST['pwd'];
-    $query = "SELECT * from login where username ='$username'";
-    
+    $user_password = $_POST['pwd'];
 
+    $query = "SELECT * from user where username ='$username'";
+    #checks if the first name of a professor is in the database but you want to check all 
     if ($result=mysqli_query($conn,$query))
     {
         if(mysqli_num_rows($result) > 0)
@@ -27,8 +37,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             echo "Exists";
         }
         else{
-            $sql = "INSERT INTO login (first_name, last_name, username, password)
-            VALUES ('$first_name', '$last_name', '$username', '$password')";
+            $sql = "INSERT INTO user (first_name, last_name, username,user_password)
+            VALUES ('$first_name', '$last_name', '$username', '$user_password')";
             if ($conn->query($sql) === TRUE) {
                 header('location: http://localhost/WebPL/home.html');
             } else {
@@ -46,6 +56,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 
+    $conn->close();
 
-};
+    
+
+
+    //echo 'Your information has been submitted';
+}
+
 ?>
